a79586a1c18ef7549d8420a1d9d36c71f59c412e
Kaya-Sem <kaya-semvc@hotmail.com>
2023-10-03 21:56:21 +0200

    App updates
    testing with Item View

--- app/src/main/java/com/example/rooftopper/MainActivity.kt
+++ app/src/main/java/com/example/rooftopper/MainActivity.kt
@@ -8,25 +8,19 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.rooftopper.ui.theme.RooftopperTheme
@@ -70,25 +64,6 @@ fun ItemCard() {
 }
 
 
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun addItem() {
-var text by remember {mutableStateOf(TextFieldValue(""))}
-    
-    TextField(
-        value = text, onValueChange = {newText -> text = newText},
-        label = { Text(text = "title")},
-        singleLine = true,
-        modifier = Modifier
-            .fillMaxWidth(),
-        placeholder = { Text(text = "bv. Fluvius kantoren m. kraan")}
-
-
-    )
-    
-
-}
-
 
 @Preview
 @Composable
@@ -99,9 +74,27 @@ fun itemFullScreen(title: String = "Constructiesite m. kraan") {
             .background(Color.White)
             .padding(5.dp)
     ) {
-        Image(painter = painterResource(id = R.drawable.construction), contentDescription = "", contentScale = ContentScale.Crop)
+
+        Card (shape = RoundedCornerShape(5.dp)){
+            Image(
+                painter = painterResource(id = R.drawable.construction),
+                contentDescription = "",
+                contentScale = ContentScale.Crop
+            )
+        }
         Text(text = title, fontWeight = FontWeight.ExtraBold)
+
         Text(text = "Nieuwpoortstraat 15, 9000 Gent")
+
+
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Text(text = "Information")
+        Text(text = "3 months, 5days old  (added 15/02/2023)")
+
+        Spacer(modifier = Modifier.height(300.dp))
+        Text(text = "Comments")
     }
 
 
