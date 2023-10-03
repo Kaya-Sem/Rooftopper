6aab5143959c8555ddd82f430c74da3f29fb5004
Kaya-Sem <kayasemvc@gmail.com>
2023-10-03 14:07:02 +0200

    testing things out

--- app/src/main/java/com/example/rooftopper/MainActivity.kt
+++ app/src/main/java/com/example/rooftopper/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.ModifierLocalScrollableContainerProvider.value
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -12,8 +13,11 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.rooftopper.ui.theme.RooftopperTheme
@@ -29,7 +33,7 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-@Preview(showBackground = true)
+
 @Composable
 fun ItemCard() {
     RooftopperTheme {
@@ -50,4 +54,18 @@ fun ItemCard() {
     }
 }
 
+
+@Preview
+@Composable
+fun addItem() {
+    // Aks location permission if not already
+var text by remember {mutableStateOf(TextFieldValue(""))}
+    TextField(
+        value = text,
+        onValueChange = { newText -> text = newText},
+        label = { Text(text = "Location name")},
+        placeholder = { Text(text = "Boudewijnlaan 10, 9000 Gent")}
+    ) {
+    }
+}
 
