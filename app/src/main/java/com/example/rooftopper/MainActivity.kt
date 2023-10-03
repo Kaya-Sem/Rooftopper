da5059298ffdb56cf37bfc1ad8b12aea3539c241
Kaya-Sem <kayasemvc@gmail.com>
2023-10-03 13:55:01 +0200

    testing things out

--- app/src/main/java/com/example/rooftopper/MainActivity.kt
+++ app/src/main/java/com/example/rooftopper/MainActivity.kt
@@ -3,13 +3,19 @@ package com.example.rooftopper
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.rooftopper.ui.theme.RooftopperTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,26 +24,30 @@ class MainActivity : ComponentActivity() {
         setContent {
             RooftopperTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
+
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun ItemCard() {
     RooftopperTheme {
-        Greeting("Android")
+        Card (
+
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(50.dp)
+                .padding(15.dp)
+                .clickable { } // #TODO
+        ){
+            Text(text = "Titel")
+            Text(text = "adress")
+
+
+        }
+    }
     }
 }
+
+
